99c11a868664f99138045b594c6814abfdec4e6c
aarontasker <aaron.tasker@gmail.com>
2021-11-22 09:58:21 +0100

    Added migrations directory

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,20 @@ from jaaql.jaaql import create_app
 from controller import JAAQLHelloWorldController
 import documentation as documentation
 
+# from os.path import join, dirname
+# migration_folder = join(dirname(__file__), "migrations")
+# migration_project_name = "example"
+
+
 if __name__ == '__main__':
-    port, flask_app = create_app(supplied_documentation=documentation, controllers=[JAAQLHelloWorldController()])
-    flask_app.run(port=port, host="0.0.0.0", threaded=True)
+    port, flask_app = create_app(supplied_documentation=documentation, controllers=[JAAQLHelloWorldController()],
+                                 # migration_folder=migration_folder,
+                                 # migration_project_name=migration_project_name
+                                 )
+    flask_app.run(port=port, host="0.0.0.0", threaded=True,)
 else:
     def build_app(*args, **kwargs):
         return create_app(is_gunicorn=True, supplied_documentation=documentation,
+                          # migration_folder=migration_folder,
+                          # migration_project_name=migration_project_name,
                           controllers=[JAAQLHelloWorldController()], **kwargs)
